4ce1fb21cd44872cc5c0c66c8005aaa0c340204f
BlossomEze <blossomeze0@gmail.com>
2023-04-25 20:13:59 -0700

    main

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,16 @@
-#include "main.h"
-/**
- * main - print _putchar
- * is written by mbah
- * Return: 0
-*/
+#include <stdio.h>
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return 0;
 }
